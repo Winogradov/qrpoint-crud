ca50c528eff2d18908fa46d8399c929ef5fcba5d
izumlenn2000 <vladislavs-v@yandex.ru>
2020-12-27 17:36:47 +0300

    Initial commit

---
+++ src/main/java/com/example/qrpointcrud/QrpointCrudApplication.java
@@ -0,0 +1,13 @@
+package com.example.qrpointcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrpointCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrpointCrudApplication.class, args);
+	}
+
+}
